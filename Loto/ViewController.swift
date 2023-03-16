fc68444cc133d52edf7292a8dd80d8fe3bc14e0f
YanChagas <yancoutinhodch@gmail.com>
2023-03-16 20:25:49 -0300

    Initial Commit

---
+++ Loto/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Loto
+//
+//  Created by Jessica Haber on 16/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbGame: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    
+    @IBOutlet var balls: [UIButton]!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func generateGame() {
+    }
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
